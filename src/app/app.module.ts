8a8f2467b1202761123e489523ad60d1fba67ebe
YesicaChui <yesicachuic@gmail.com>
2023-10-04 19:04:35 -0500

    iniciando angular con formularios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistroAlumnosComponent } from './components/registro-alumnos/registro-alumnos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistroAlumnosComponent
   ],
   imports: [
     BrowserModule,
